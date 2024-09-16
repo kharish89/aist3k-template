29a0b7c26897e4e33828b13328a49d56c1336080
Xe Iaso <me@xeiaso.net>
2024-09-16 17:23:28 -0400

    match the example
    Signed-off-by: Xe Iaso <me@xeiaso.net>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
-export default function Home() {
-  return <>TODO: everything</>;
+"use client";
+
+import { TigrisObject } from "@/lib/tigris";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import listFiles from "@/app/actions/files";
+
+export default function VideoList() {
+  return <>TODO: implement</>;
 }
--- src/components/VideoList.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { TigrisObject } from "@/lib/tigris";
-import Link from "next/link";
-import { useState, useEffect } from "react";
-
-export default function VideoList() {
-  return <>TODO: implement</>;
-}
